7a543e0ee0ae5d3b447abbcc02052aa1c556f218
Richard <richard@communiteq.com>
2023-02-21 16:08:13 +0000

    Restrict to journal entries only

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # name: discourse-topic-thumbnail-recent-post
-# about: Set topic thumbnail to most recent post of topic starter
-# version: 1.0
+# about: Set topic thumbnail to most recent journal entry of topic starter
+# version: 1.1
 # authors: Communiteq
 # url: https://github.com/communiteq/discourse-topic-thumbnail-recent-post
 
@@ -11,8 +11,8 @@ enabled_site_setting :topic_thumbnail_recent_post_enabled
 after_initialize do
   DiscourseEvent.on(:post_process_cooked) do |doc, post|
     if SiteSetting.topic_thumbnail_recent_post_enabled? 
-      # if the post is a journal then it must be an entry --> the post is not a journal, or it is an entry
-      if post.topic && post.topic&.user&.id == post&.user&.id && post.image_upload_id && (!post.respond_to?(:journal?) || !post.journal? || post.entry?)
+      # the post must be a journal and it must be an entry
+      if post.topic && post.topic&.user&.id == post&.user&.id && post.image_upload_id && post.respond_to?(:journal?) && post.journal? && post.entry?
         post.topic.image_upload_id = post.image_upload_id
         post.topic.save!
       end
