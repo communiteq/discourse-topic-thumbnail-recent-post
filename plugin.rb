2800f61298255a877edfb145e577dc0a25632d33
Richard <richard@communiteq.com>
2024-10-02 15:16:40 +0000

    Drop journal requirement; add category setting

--- plugin.rb
+++ plugin.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 # name: discourse-topic-thumbnail-recent-post
-# about: Set topic thumbnail to most recent journal entry of topic starter
-# version: 1.1
+# about: Set topic thumbnail to most recently updated post of topic starter
+# version: 1.2
 # authors: Communiteq
 # url: https://github.com/communiteq/discourse-topic-thumbnail-recent-post
 
 enabled_site_setting :topic_thumbnail_recent_post_enabled
 
 after_initialize do
+  Category.register_custom_field_type("enable_thumbnail_recent_post", :boolean)
+  Site.preloaded_category_custom_fields << "enable_thumbnail_recent_post"
+
   DiscourseEvent.on(:post_process_cooked) do |doc, post|
-    if SiteSetting.topic_thumbnail_recent_post_enabled? 
-      # the post must be a journal and it must be an entry
-      if post.topic && post.topic&.user&.id == post&.user&.id && post.image_upload_id && post.respond_to?(:journal?) && post.journal? && post.entry?
+    if SiteSetting.topic_thumbnail_recent_post_enabled?
+      if post.topic && post.topic&.user&.id == post&.user&.id && post.image_upload_id && post.topic.category && post.topic.category.custom_fields[:enable_thumbnail_recent_post]
         post.topic.image_upload_id = post.image_upload_id
         post.topic.save!
       end
